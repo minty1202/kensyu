a559538e6b0affdc31c291d207fd7312b060c5b6
kazufoot21 <kazufoot21@outlook.jp>
2022-05-11 16:18:10 +0900

    [update]#2 DB作り直し

--- db/migrate/20220509063334_devise_create_admins.rb
+++ db/migrate/20220509063334_devise_create_admins.rb
@@ -7,7 +7,6 @@ class DeviseCreateAdmins < ActiveRecord::Migration[7.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      t.string :name,               null: false
 
       ## Recoverable
       t.string   :reset_password_token
