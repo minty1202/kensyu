aeece7a41c894b049342d7979dabbd6f1ea45481
kazufoot21 <kazufoot21@outlook.jp>
2022-05-17 17:17:33 +0900

    [update]#6 ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :users do
     resource :mypage, only: :show
-    resource :todo, only:[:new, :create]
+    resources :todos, only:[:new, :create, :edit, :update]
   end
 
 
