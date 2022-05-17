a620993e84c78ac881b46eaa505bfbd57f292d0f
kazufoot21 <kazufoot21@outlook.jp>
2022-05-17 17:19:08 +0900

    [update] 編集機能作成

--- app/controllers/users/todos_controller.rb
+++ app/controllers/users/todos_controller.rb
@@ -13,6 +13,21 @@ class Users::TodosController < UsersController
     end
   end
 
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(title: todo_params[:title], text: todo_params[:text], user_id: current_user.id)
+      flash[:success] = "Todoを更新しました！"
+      redirect_to users_mypage_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
     def todo_params
       params.require(:todo).permit(:title, :text)
