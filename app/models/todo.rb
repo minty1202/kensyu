1c42d7bd7fc8bf562c30763864aa95dec12e3b49
kazufoot21 <kazufoot21@outlook.jp>
2022-05-19 13:20:03 +0900

    [update]#10 画像投稿機能

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,14 @@
 class Todo < ApplicationRecord
   validates :title, presence: true, length: {maximum: 50}
   validates :text, presence: true
+  validate :file_length
+  has_many_attached :images
   belongs_to :user
+
+  private
+    def file_length
+      if images.length > 3
+        errors.add(:images, 'は 3 ファイルまでにしてください')
+      end
+    end
 end
