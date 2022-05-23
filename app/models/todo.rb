9d13ec5422511244e86f2d6b2a8429eadbe235aa
kazufoot21 <kazufoot21@outlook.jp>
2022-05-23 15:37:47 +0900

    [update]#10 画像リサイズ

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,7 +2,9 @@ class Todo < ApplicationRecord
   validates :title, presence: true, length: {maximum: 50}
   validates :text, presence: true
   validate :file_length
-  has_many_attached :images
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
   belongs_to :user
 
   private
