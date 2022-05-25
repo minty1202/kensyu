a3771b9e0e9b6b8439ecae47e9de36bc0c529dd4
kazufoot21 <kazufoot21@outlook.jp>
2022-05-25 12:42:40 +0900

    [update]#11 ユーザー削除 resources→resource

--- app/controllers/admins/dashbords_controller.rb
+++ app/controllers/admins/dashbords_controller.rb
@@ -5,16 +5,7 @@ class Admins::DashbordsController < AdminsController
   end
 
   def destroy
-    puts '-----------------------------------'
-    puts '-----------------------------------'
-    pp params
-    @user = User.find(params[:id])
-    puts '-----------------------------------'
-    puts '-----------------------------------'
-    pp @user
-    puts '-----------------------------------'
-    puts '-----------------------------------'
-    user = User.find(params[:id])
+    user = User.find(params[:format])
     user.destroy
     redirect_to admins_dashbord_path
   end
