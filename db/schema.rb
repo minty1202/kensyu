dcc985fef5ffbb7276a0a5799a4fd3eefc2eff0b
kazufoot21 <kazufoot21@outlook.jp>
2022-06-01 15:26:07 +0900

    [update]#17 user, tagアソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_084617) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
