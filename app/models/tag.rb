36c414fbfa66e7ffdc51446b0941c699a0461a1b
kazufoot21 <kazufoot21@outlook.jp>
2022-06-13 16:29:10 +0900

    [update]#21  タグ順変更

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,11 @@ class Tag < ApplicationRecord
   belongs_to :user
   has_many :todo_tags, dependent: :destroy, foreign_key: 'tag_id'
   has_many :todos, through: :todo_tags
+
+  def self.hoge
+    select('tags.*', 'count(todo_tags.id) AS todo_tags')
+      .left_joins(:todo_tags)
+      .group('id')
+      .order('todo_tags desc')
+  end
 end
