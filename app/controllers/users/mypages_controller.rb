8d1528c5a96ab7667c4b576765c43e1af23092e7
kazufoot21 <kazufoot21@outlook.jp>
2022-06-13 17:53:58 +0900

    [update]#21 コードレビュー修正

--- app/controllers/users/mypages_controller.rb
+++ app/controllers/users/mypages_controller.rb
@@ -2,7 +2,7 @@ module Users
   class MypagesController < UsersController
     def show
       @todos = current_user.todos.order(limit_date: "ASC").page(params[:page]).per(8)
-      @tags = current_user.tags.hoge  # ビューでタグ一覧を表示するために全取得。
+      @tags = current_user.tags.change_tag_order  # ビューでタグ一覧を表示するために全取得。
     end
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,7 +5,7 @@ class Tag < ApplicationRecord
   has_many :todo_tags, dependent: :destroy, foreign_key: 'tag_id'
   has_many :todos, through: :todo_tags
 
-  def self.hoge
+  def self.change_tag_order
     select('tags.*', 'count(todo_tags.id) AS todo_tags')
       .left_joins(:todo_tags)
       .group('id')
