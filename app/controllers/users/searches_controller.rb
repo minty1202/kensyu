6c8bceb1837f70732f07a58ea3c833d74a0d436b
kazufoot21 <kazufoot21@outlook.jp>
2022-06-16 15:40:22 +0900

    [update]#23 コードレビュー修正

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -1,10 +1,9 @@
 module Users
   class SearchesController < UsersController
     def search
-      keywords = params[:keyword].split(/[[:blank:]]+/).select(&:present?)
-
-      @todos = keywords.map { |keyword| Todo.search_title(keyword).or(Todo.search_text(keyword)) }
-      @todos = @todos.flatten.uniq
+      keywords = params[:keyword].split(/\s/)
+      @todos = keywords.map { |keyword| Todo.search_title(keyword).or(Todo.search_text(keyword)) }.flatten.uniq
+      @todos = @todos.sort { |a, b| a[:limit_date] <=> b[:limit_date] }
     end
   end
 end
