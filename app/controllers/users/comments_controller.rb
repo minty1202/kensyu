649d8cd99ba757a5e4c6b385b34afaa9893e2155
kazufoot21 <kazufoot21@outlook.jp>
2022-06-23 12:27:55 +0900

    [udpate]#24 コメント機能バグ修正

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -2,8 +2,13 @@ module Users
   class CommentsController < ApplicationController
     def create
       @comment = current_user.comments.new(comment_params)
-      @comment.save
-      redirect_to edit_users_todo_path(@comment.todo)
+      if @comment.save
+        redirect_to edit_users_todo_path(@comment.todo)
+        flash[:success] = "コメントの登録が成功しました！"
+      else
+        @todo = current_user.todos.find(params[:id])
+        render @todo, status: :unprocessable_entity
+      end
     end
 
     private
