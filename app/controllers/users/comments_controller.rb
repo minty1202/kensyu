eba3ec02302af15f09cb31c1e1383eaf809f009f
minty1202 <aki.men1202@gmail.com>
2022-06-23 15:43:43 +0900

    参考

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -1,9 +1,13 @@
 module Users
   class CommentsController < ApplicationController
     def create
-      @comment = current_user.comments.new(comment_params)
-      @comment.save
-      redirect_to edit_users_todo_path(@comment.todo)
+      @comment = Comment.new(comment_params)
+      if @comment.save
+        redirect_to edit_users_todo_path(@comment.todo)
+      else
+        @todo = Todo.find(comment_params[:todo_id])
+        render '/users/todos/edit', status: :unprocessable_entity
+      end
     end
 
     private
