22c31b3ba83a8a49e6624bde57f40924e0f17d66
kazufoot21 <kazufoot21@outlook.jp>
2022-07-01 11:49:42 +0900

    [udpate]# 34 データが存在しないページの場合リダイレクトさせる

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,5 +1,16 @@
 class GeneralController < ApplicationController
+  include JudgeParams
+
   def index
-    @todos = Todo.order(limit_date: "ASC").includes(:user).page(params[:page]).per(8)
+    @set_todos = Todo.order(limit_date: "ASC").includes(:user)
+    @todos = @set_todos.page(params[:page]).per(8)
+    judge_params
+  end
+
+  private
+
+  def no_page
+    redirect_to "/"
+    flash[:notice] = "データが見つかりませんでした。"
   end
 end
--- app/controllers/users/mypages_controller.rb
+++ app/controllers/users/mypages_controller.rb
@@ -1,8 +1,19 @@
 module Users
   class MypagesController < UsersController
+    include JudgeParams
+
     def show
-      @todos = current_user.todos.order(limit_date: "ASC").page(params[:page]).per(8)
+      @set_todos = current_user.todos.order(limit_date: "ASC")
+      @todos = @set_todos.page(params[:page]).per(8)
       @tags = current_user.tags.change_tag_order  # ビューでタグ一覧を表示するために全取得。
+      judge_params
+    end
+
+    private
+
+    def no_page
+      redirect_to "/users/mypage"
+      flash[:notice] = "データが見つかりませんでした。"
     end
   end
 end
