22f637b25e739ed19d88a1c1e78fab63b4ffffc8
kazufoot21 <kazufoot21@outlook.jp>
2022-07-22 11:41:07 +0900

    [update]#45 remove_attr_reader

--- app/forms/tag_form.rb
+++ app/forms/tag_form.rb
@@ -3,11 +3,10 @@ class TagForm
   include ActiveModel::Attributes
 
   attr_accessor :name, :user_id
-  attr_reader :tag
 
   validates :name, presence: true, length: { maximum: 10 }
 
-  delegate :persisted?, to: :tag
+  delegate :persisted?, to: :@tag
 
   def initialize(attributes = nil, tag: Tag.new)
     @tag = tag
@@ -19,22 +18,22 @@ class TagForm
     return if invalid?
 
     ActiveRecord::Base.transaction do
-      tag.update!(name:, user_id:)
+      @tag.update!(name:, user_id:)
     end
   rescue ActiveRecord::RecordInvalid
     false
   end
 
   def to_model
-    tag
+    @tag
   end
 
   private
 
   def default_attributes
     {
-      name: tag.name,
-      user_id: tag.user_id
+      name: @tag.name,
+      user_id: @tag.user_id
     }
   end
 end
