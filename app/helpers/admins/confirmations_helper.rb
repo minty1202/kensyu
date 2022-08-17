058e65c69fae9350353f52befd4167a6268ceace
kazufoot21 <kazufoot21@outlook.jp>
2022-08-17 16:14:23 +0900

    [update]#48 fix slim lint

---
+++ app/helpers/admins/confirmations_helper.rb
@@ -0,0 +1,12 @@
+module Admins
+  module ConfirmaitonsHelper
+
+    def confirmed_email?(resource)
+      if resource.pending_reconfirmation?
+        resource.unconfirmed_email
+      else
+        resource.email
+      end
+    end
+  end
+end
