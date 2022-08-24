86588f68e268167070fe8689c275e70f10feb68c
kazufoot21 <kazufoot21@outlook.jp>
2022-08-24 09:21:31 +0900

    [update]#47 fix lint factories

--- spec/factories/forms/todo_tag_form.rb
+++ spec/factories/forms/todo_tag_form.rb
@@ -1,14 +1,17 @@
 FactoryBot.define do
   factory :todo_tag_form do
-    title {'title'}
-    text {'text'}
-    limit_date {Time.current}
-    status {'未完了'}
-    name {'name'}
+    title { 'title' }
+    text { 'text' }
+    limit_date { Time.current }
+    status { '未完了' }
+    name { 'name' }
 
     trait :with_attachment do
-      attachment { Rack::Test::UploadedFile.new(
-      "#{Rails.root}/spec/fixtures/files/image/test_image.png", 'image/png') }
+      attachment do
+        Rack::Test::UploadedFile.new(
+          "#{Rails.root}/spec/fixtures/files/image/test_image.png", 'image/png'
+        )
+      end
     end
   end
 end
